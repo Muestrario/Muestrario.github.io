9af4756217c384f8a39c7b8f2d199f8a87aca480
Renzo Soncco <rencitotupapa30@gmail.com>
2021-06-05 20:51:15 -0500

    prueba 1

---
+++ funciones.js
@@ -0,0 +1,10 @@
+function lista_de_minerales(numero, palabra) {
+    ul>
+        <li class = "toclevel-numero tocsection-numero">
+        <a href= #palabra>
+            <span class= "tocnumber">numero</span>
+            <span class="toctext">palabra</span>
+        </a>
+     </ul>
+}
+function lista_de_minerales(1, Elementos nativos)
